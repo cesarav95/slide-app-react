72d9ba64b732bf9f32f9ca2a4d216171e7fbfdd9
cesarav95 <cesarav751323@gmail.com>
2021-06-03 22:19:43 -0500

    new update

--- src/components/Slides.js
+++ src/components/Slides.js
@@ -1,66 +1,48 @@
 import React, { useState } from 'react';
 
-function Slides({slides}) {
+function Slides({slides}) { 
 
-    const [disPrev, setDisPre] = useState(true);
-    const [disRest, setDisRest] = useState(true);
-    const [disNext, setDisNext] = useState(false);
     const [nroSlide, setNroSlide] = useState(0);
 
-    /*const [stateSlides, setSateSlides] = useState({
-                                            prev:true,
-                                            rest:true,
-                                            next:false,
-                                            nro:0
-                                        }); */
+    const restartSlides = () => {
+        setNroSlide(0);
+    }
+    const previousSlide = () => {
+        if (nroSlide > 0) {
+            setNroSlide(nroSlide - 1);
+        }        
+    }
+    const nextSlide = () => {
+        if (nroSlide < slides.length - 1) {
+            setNroSlide(nroSlide + 1);
+        }
+    }
+
     return (
         <div>
             <div id="navigation" className="text-center">
                 <button data-testid="button-restart" className="small outlined" 
-                    disabled={disRest}
-                    onClick={() => { 
-                        setDisPre(true); 
-                        setDisRest(true);
-                        setDisNext(false);
-                        setNroSlide(0);
-                    }}
-                >Restart</button>
+                    disabled={nroSlide === 0}
+                    onClick={restartSlides}
+                >
+                    Restart
+                </button>
+
                 <button data-testid="button-prev" className="small" 
-                    disabled={disPrev}
-                    onClick={() => { 
-                        if (nroSlide > 0) {
-                            let previousSlide = nroSlide - 1
-                            setNroSlide(previousSlide);
-                            if (previousSlide < slides.length - 1) {
-                                if (disNext) setDisNext(false);
-                            }
-                            if ( previousSlide == 0){
-                                setDisPre(true);
-                                setDisRest(true);
-                            }
-                        } 
-                    }}
-                >Prev</button>
+                    disabled={nroSlide === 0}
+                    onClick={previousSlide}
+                >
+                    Prev
+                </button>
+
                 <button data-testid="button-next" className="small" 
-                    disabled={disNext}
-                    onClick={() => { 
-                        if (nroSlide < slides.length) {
-                            let nextSlide = nroSlide + 1;
-                            setNroSlide(nextSlide);
-                            if (nextSlide == slides.length - 1 ) {
-                                setDisNext(true);
-                                setDisPre(false);
-                                setDisRest(false);   
-                            } else {
-                                if (nextSlide > 0 ){
-                                    if (disPrev) setDisPre(false);
-                                    if (disRest) setDisRest(false);
-                                }
-                            }                                                 
-                        } 
-                    }}
-                >Next</button>
+                    disabled={nroSlide === slides.length - 1}
+                    onClick={nextSlide}
+                >
+                    Next
+                </button>
             </div>
+
             <div id="slide" className="card text-center">
                 <h1 data-testid="title">{slides[nroSlide].title}</h1>
                 <p data-testid="text">{slides[nroSlide].text}</p>
